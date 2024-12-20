77fae33ab950567945fdb40310591224d62d16df
sharon <ali93456@gmail.com>
2024-12-20 18:43:34 +0300

    add subcommand and flag completion

--- cmd/root.go
+++ cmd/root.go
@@ -29,4 +29,63 @@ func Execute() {
 
 func init() {
 	rootCmd.PersistentFlags().StringVarP(&filePath, "file", "f", "", "File to be manipulated")
+
+	rootCmd.AddCommand(completionCmd)
+}
+
+var completionCmd = &cobra.Command{
+	Use:   "completion",
+	Short: "Generates bash completion scripts",
+	Long: `To load completions:
+
+Bash:
+
+$ source <(memepulation completion bash)
+
+# To load completions for each session, execute once:
+Linux:
+  $ memepulation completion bash > /etc/bash_completion.d/memepulation
+MacOS:
+  $ memepulation completion bash > /usr/local/etc/bash_completion.d/memepulation
+
+Zsh:
+
+$ source <(memepulation completion zsh)
+
+# To load completions for each session, execute once:
+$ memepulation completion zsh > "${fpath[1]}/_memepulation"
+
+Fish:
+
+$ memepulation completion fish | source
+
+# To load completions for each session, execute once:
+$ memepulation completion fish > ~/.config/fish/completions/memepulation.fish
+
+Powershell:
+
+PS> memepulation completion powershell | Out-String | Invoke-Expression
+
+# To load completions for every new session, run:
+PS> memepulation completion powershell > memepulation.ps1
+# and source this file from your PowerShell profile.
+`,
+	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) < 1 {
+			cmd.Help()
+			return
+		}
+		        switch args[0] {
+        case "bash":
+            rootCmd.GenBashCompletion(os.Stdout)
+        case "zsh":
+            rootCmd.GenZshCompletion(os.Stdout)
+        case "fish":
+            rootCmd.GenFishCompletion(os.Stdout, true)
+        case "powershell":
+            rootCmd.GenPowerShellCompletion(os.Stdout)
+		default:
+			cmd.Help()
+		}
+	},
 }
--- install.sh
+++ install.sh
@@ -22,6 +22,27 @@ curl -L -o /usr/local/bin/$BINARY_NAME "$DOWNLOAD_URL"
 
 chmod +x /usr/local/bin/$BINARY_NAME
 
+
+# Check user shell and setup autocompletion
+shell_name=$(basename "$SHELL")
+
+case "$shell_name" in
+  bash)
+      source <(memepulation completion bash)
+    ;;
+  zsh)
+      source <(memepulation completion zsh)
+    ;;
+  fish)
+      memepulation completion fish | source
+    ;;
+  *)
+    echo "Brother, what type of shell are you using?"
+    exit 1
+    ;;
+esac
+
+
 if command -v $BINARY_NAME > /dev/null 2>&1; then
   echo "$BINARY_NAME installed successfully!"
 else
